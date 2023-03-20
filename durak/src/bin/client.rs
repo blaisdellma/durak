d8e938425dc00aaea6309349b58ca2cc43f27bae
Matt Blaisdell <blaisdellma@gmail.com>
2023-03-20 13:43:21 -0400

    Separate server/client executables

---
+++ durak/src/bin/client.rs
@@ -0,0 +1,41 @@
+use tracing::{info,debug,warn,Level};
+use tracing_subscriber as ts;
+use tracing_appender as ta;
+
+use durak_core::prelude::*;
+use durak_players::*;
+
+fn init_log(prefix: &str) -> DurakResult<ta::non_blocking::WorkerGuard> {
+    let log_dir = std::env::var("CARGO_MANIFEST_DIR")?;
+    let (file, guard) = ta::non_blocking(ta::rolling::daily(log_dir,prefix));
+    ts::fmt()
+        .with_writer(file)
+        .with_max_level(Level::DEBUG)
+        .with_env_filter({
+            ts::EnvFilter::from_default_env()
+                .add_directive("durak=debug".parse()?)
+                .add_directive("cursive=warn".parse()?)
+        }).init();
+    debug!("Log init successful");
+    Ok(guard)
+}
+
+fn run_game_client() -> DurakResult<()> {
+    let _guard = init_log("client_log").map_err(|e| { warn!("Log init failed"); e })?;
+    let mut player = NetClientDurakPlayer::new(CliPlayer::new(0))?;
+    info!("Connected to game server");
+    loop {
+        match player.wait()? {
+            1 => { break; },
+            2 => { break; },
+            _ => {},
+        }
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run_game_client() {
+        eprintln!("ERROR: {}",e);
+    }
+}
---
+++ durak/src/bin/server.rs
@@ -0,0 +1,47 @@
+use rand::thread_rng;
+use tracing::{info,debug,warn,error,Level};
+use tracing_subscriber as ts;
+use tracing_appender as ta;
+
+use durak_core::prelude::*;
+use durak_players::*;
+
+fn init_log(prefix: &str) -> DurakResult<ta::non_blocking::WorkerGuard> {
+    let log_dir = std::env::var("CARGO_MANIFEST_DIR")?;
+    let (file, guard) = ta::non_blocking(ta::rolling::daily(log_dir,prefix));
+    ts::fmt()
+        .with_writer(file)
+        .with_max_level(Level::DEBUG)
+        .with_env_filter({
+            ts::EnvFilter::from_default_env()
+                .add_directive("durak=debug".parse()?)
+                .add_directive("cursive=warn".parse()?)
+        }).init();
+    debug!("Log init successful");
+    Ok(guard)
+}
+
+fn run_game_server() -> DurakResult<()> {
+    let _guard = init_log("server_log").map_err(|e| { warn!("Log init failed"); e })?;
+    let mut game = DurakGame::new();
+
+    let mut server = DurakServer::new()?;
+    for _ in 0..3 {
+        server.wait_connection()?;
+        info!("Client connected to server");
+    }
+    for player in server.get_players()? {
+        game.add_player(Box::new(player))?;
+    }
+
+    game.init(&mut thread_rng()).map_err(|e| { error!("Game initialization error: {}",e); e })?;
+    game.run_game().map_err(|e| { error!("Game error: {}",e); e })?;
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run_game_server() {
+        eprintln!("ERROR: {}",e);
+    }
+}
